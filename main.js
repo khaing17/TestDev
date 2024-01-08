a986bef16ae9d0fcb7c3a1a194ab6654b35f67a9
khaing17 <aungthetkhaing017@gmail.com>
2024-01-08 22:13:40 +0630

    fix

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ const menuBtn = document.getElementById("menu-btn");
 const closeBtn = document.getElementById("close-btn");
 const nav = document.querySelector("header");
 const heroSection = document.getElementById("section-hero");
-console.log(nav);
+const popularSection = document.getElementById("section-popular");
+console.log(popularSection);
+const topOfPopularSection = popularSection?.offsetTop;
+console.log(topOfPopularSection);
 
 /**For Menu Functionality */
 const openMenu = () => {
   menu?.classList.remove("w-0", "h-0", "opacity-0");
   menu?.classList.add("w-screen", "h-screen", "opacity-100");
+  console.log("open");
 };
 
-function closeMenu() {
+const closeMenu = () => {
   menu?.classList.remove("w-screen", "h-screen", "opacity-100");
   menu?.classList.add("w-0", "h-0", "opacity-0");
-}
+};
+
+menuBtn?.addEventListener("click", openMenu);
+closeBtn?.addEventListener("click", closeMenu);
 
 /**For Sticky Navbar */
 const navHeight = nav.getBoundingClientRect().height;
@@ -23,9 +30,9 @@ console.log(navHeight);
 const stickyNav = (entries) => {
   const [entry] = entries;
   if (!entry.isIntersecting) {
-    nav?.classList.add("fixed", "top-0", "bg-white", "w-full");
+    nav?.classList.add("sticky", "top-0", "bg-white");
   } else {
-    nav?.classList.remove("fixed", "top-0", "bg-white");
+    nav?.classList.remove("sticky", "top-0", "bg-white");
   }
   console.log(nav?.classList);
   console.log(nav?.clientWidth);
@@ -37,3 +44,21 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(heroSection);
+/**For Smooth Scroll*/
+const navLinks = document.querySelectorAll(".nav-link");
+console.log(navLinks);
+
+navLinks.forEach((navLink) =>
+  navLink.addEventListener("click", (e) => {
+    e.preventDefault(); // Prevent the default behavior of the link
+
+    const id = e.currentTarget.getAttribute("href");
+    const targetElement = document.querySelector(id);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  })
+);
